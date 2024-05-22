6587749b5f90575f6176f1334e01c952171cd912
Maxim25252525 <149468309+Maxim25252525@users.noreply.github.com>
2024-05-22 10:03:39 +0300

    Исправил getData

--- public/scripts/api-interactors.js
+++ public/scripts/api-interactors.js
@@ -7,7 +7,10 @@ const getJWTFromCookie = () => {
 
 const getData = async url => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'no-cors',
+    });
     const data = await response.json();
     return data;
   } catch (err) {
